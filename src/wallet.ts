6094a6f8371057f6ac2b8f5bfcb9656c26e5c3d9
Maxime <maxime.dharboulle@gmail.com>
2021-12-09 14:02:13 +0100

    fix

--- src/wallet.ts
+++ src/wallet.ts
@@ -16,29 +16,28 @@ export class Wallet {
 
     public static async computeValue(wallet: Wallet, returnValueCurrency: Currencies = Currencies.EUR, date: string = 'latest'): Promise<number> {
         const exchangeRateApiUrl = `http://api.exchangeratesapi.io/${date}`
+        
+        return new Promise((resolve, reject) => {
+            let total: number = 0
+            const searchCurrencies: string = wallet.stocks.filter(stock => stock.currency !== returnValueCurrency).map(stock => stock.currency).join(',')
+            axios.get(`${exchangeRateApiUrl}?access_key=${process.env.API_KEY}&base=${returnValueCurrency}&symbols=${searchCurrencies}`).then(response => {
+                if(!response.data.success){
+                    reject(Error(response.data.error.info + "\n\tat:\n\t" + response.config.url))
+                }
+                const rawExchangeRatesRatios = response.data.rates
+                const exchangeRatesRatios = new Map<string, number>(Object.entries(rawExchangeRatesRatios));
+                for (let item of exchangeRatesRatios) {
+                    exchangeRatesRatios.set(item[0], parseFloat(item[1].toFixed(2)));
+                }
+                for(let stock of wallet.stocks){
+                    total += parseFloat((stock.count / (stock.currency === returnValueCurrency ? 1 : exchangeRatesRatios.get(stock.currency) || 0)).toFixed(2))
+                }
+                resolve(parseFloat(total.toFixed(2)))
+            }).catch(err => {
+                reject(err)
+            })
+        })
     
-        let total: number = 0
-        const searchCurrencies: string = wallet.stocks.filter(stock => stock.currency !== returnValueCurrency).map(stock => stock.currency).join(',')
-        try {
-            const response = await axios.get(`${exchangeRateApiUrl}?access_key=${process.env.API_KEY}&base=${returnValueCurrency}&symbols=${searchCurrencies}`)
-            if(!response || !response.data || !response.data.success){
-                throw new Error("Api call failed!")
-            }
-            
-            const rawExchangeRatesRatios = response.data.rates
-            const exchangeRatesRatios = new Map<string, number>(Object.entries(rawExchangeRatesRatios));
-            for (let item of exchangeRatesRatios) {
-                exchangeRatesRatios.set(item[0], parseFloat(item[1].toFixed(2)));
-            }
-            for(let stock of wallet.stocks){
-                total += parseFloat((stock.count / (stock.currency === returnValueCurrency ? 1 : exchangeRatesRatios.get(stock.currency) || 0)).toFixed(2))
-            }
-            return parseFloat(total.toFixed(2))
-        } catch (error) {
-            if(error.response && error.response.data && error.response.data.error)
-                throw new Error(error.response.data.error);
-            else    
-                throw error
-        }    
+        
     }
 }
